f8539206452368a0ddbc42d12b99cc3cdb5c6e15
Brent Cameron <bjcameron@gmail.com>
2020-04-30 09:35:42 -0600

    solved the palindromes problem

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -25,7 +25,9 @@ function power(a, b) {
 }
 
 function factorial(num) {
-	if(num === 0){
+	if(num < 0){
+		return -1;
+	}else if(num === 0){
 		return 1;
 	}else {
 		return num * factorial(num-1);
--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,28 @@
-const palindromes = function() {
-
+const palindromes = function(testWord) {
+    //clean up the data
+    testWord = testWord.toLowerCase();
+    let chars = testWord.split('');
+    let noSpaces = chars.filter(char => char != ' ');
+    let noPunc = noSpaces.filter(char => (char != '.' && char != ',' && char != '!' && char != '?'));
+    let clone = [...noPunc];
+    let testReverse = [...clone.reverse()];
+    let palindrome = areTheTwoArraysEqual(noPunc, testReverse);
+    return palindrome;
 }
 
+function areTheTwoArraysEqual(array1, array2) {
+    if(array1.length != array2.length){
+        return false;
+    }else {
+        for(let i = 0; i<array1.length; i++){
+            if(array1[i] !== array2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+};
+
+//palindromes('racecar');
 module.exports = palindromes
