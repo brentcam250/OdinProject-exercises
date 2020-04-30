6208f20a80cb19310415ab65143df8882d9da5a8
Brent Cameron <bjcameron@gmail.com>
2020-04-30 11:24:53 -0600

    solved the findTheOldest problem

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-let findTheOldest = function() {
-
+let findTheOldest = function(people) {
+    let oldest = people.reduce((currentOldest, person) => {
+        if(!person.yearOfDeath){
+            //they havent died yet, lets see how old they are today
+            person.yearOfDeath = new Date().getFullYear();
+        }
+        if((currentOldest.yearOfDeath - currentOldest.yearOfBirth) < (person.yearOfDeath - person.yearOfBirth)){
+            return person;
+        }else{
+            return currentOldest;
+        }
+    }, people[0]);
+    return oldest;
 }
+
 
 module.exports = findTheOldest
