e63803f6fb007ee7a555d45e6c915eb72ba6daf6
Brent Cameron <bjcameron@gmail.com>
2020-04-30 10:53:50 -0600

    solved the fibonacci problem

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,4 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(placeInSeries) {
+    let integerPlace = parseInt(placeInSeries);
+    if(integerPlace < 0 ){
+        return 'OOPS';
+    }
+    if(integerPlace === 0){
+        return 0;
+    }else if(integerPlace === 1){
+        return 1;
+    }else if (integerPlace === 2){
+        return 1;
+    }else{
+        return (fibonacci(integerPlace -1) + fibonacci(integerPlace -2));
+    }
 
 }
 
