6347a4a997dfde5a11acb67966212128500d1e8e
Brent Cameron <bjcameron@gmail.com>
2020-04-29 21:04:15 -0600

    solved the calculator problems

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,24 +1,35 @@
-function add () {
-	
+function add (a, b) {
+	return a + b;
 }
 
-function subtract () {
-	
+function subtract (a, b) {
+	return a - b;
 }
 
-function sum () {
-	
+function sum (inputArray) {
+	const sum = inputArray.reduce((total, addend) => {
+		return total += addend;
+	},0)
+	return sum;
+}
+//takes in an array, need to multiply all things in the array.
+function multiply (inputArray) {
+	const product = inputArray.reduce((product, factor)=>{
+		return product *= factor;
+	}, 1)
+	return product;
 }
 
-function multiply () {
-	
+function power(a, b) {
+	return Math.pow(a, b);
 }
 
-function power() {
-	
-}
-
-function factorial() {
+function factorial(num) {
+	if(num === 0){
+		return 1;
+	}else {
+		return num * factorial(num-1);
+	}
 	
 }
 
